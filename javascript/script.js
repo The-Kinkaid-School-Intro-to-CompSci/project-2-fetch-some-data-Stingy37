eb4da4bf4149fb17cffef37b8214d42edca66d98
Andy Shi <andyshi@Andys-MacBook-Pro.local>
2024-05-10 09:54:36 -0500

    friday's work

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+// things to do:
+// [ NONE FOR NOW ]
+// end of things to do
+
+
+// create global variables
+
+// end of global variables
+
+// connects javascript variables to  HTML elements
+function initialize_html_elements(){
+}
+
+function runProgram(){
+    console.log('runProgram');
+
+    initialize_html_elements()
+
+}
+
+document.addEventListener('DOMContentLoaded', runProgram);
